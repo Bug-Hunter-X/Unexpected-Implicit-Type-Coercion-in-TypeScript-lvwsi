434252cf044d1b3cebb91028a7005b7be64ecb7e
BugHunter-X <askzenai@gmail.com>
2024-12-29 07:38:40 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,9 @@
+function printString(str: string): void {
+  console.log(str);
+}
+
+function printNumber(num: number): void {
+  console.log(num);
+}
+
+printString(123); // This will compile and run without error, but it's logically incorrect
